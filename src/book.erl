df62905b5d697623a477c0219d99e5286dcf754a
Leon Weemen <weemen@leonweemen.nl>
2015-10-08 23:14:25 +0200

    Added include

--- src/book.erl
+++ src/book.erl
@@ -1,6 +1,8 @@
--module(book)
+-module(book).
 
 -export([open/0, close/0, write_page/1, delete_page/1, publish_page/1, unpublish_page/1])
+
+-include("commands.hrl").
 
 open() -> application:start(book).
 
