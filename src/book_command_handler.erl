cc900d66c56a9a32b5648278825da04deee69b96
Leon Weemen <weemen@leonweemen.nl>
2015-10-08 23:22:09 +0200

    Added handler functions + init function for command handler

--- src/book_command_handler.erl
+++ src/book_command_handler.erl
@@ -2,4 +2,13 @@
 -author("weemen").
 
 %% API
--export([]).
+-export([add_handler/0, delete_handler/0, init/1]).
+
+add_handler() ->
+  book_command_bus:add_handler(?MODULE, []).
+
+delete_handler() ->
+  book_command_bus:delete_handler(?MODULE, []).
+
+init([]) ->
+  {ok, []}.
