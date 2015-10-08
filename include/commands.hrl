69d1dd9ea3663a5e2df7fb803c9fc31016bb3320
Leon Weemen <weemen@leonweemen.nl>
2015-10-08 22:58:01 +0200

    Some possible base commands

---
+++ include/commands.hrl
@@ -0,0 +1,10 @@
+%%commands for command handler
+-record(write_new_draft, {id} ).
+
+-record(delete_draft, {id} ).
+
+-record(refine_draft, {id} ).
+
+-record(publish_draft, {id}).
+
+-record(unpublish_draft, {id}).
