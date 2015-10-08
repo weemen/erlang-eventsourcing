c5fb9bfc83464dad4214732bd785bc4e68a1a659
Leon Weemen <l.weemen@simgroep.nl>
2015-10-08 17:03:53 +0200

    updated possible eventnames

---
+++ src/book.erl
@@ -0,0 +1,15 @@
+-module(book)
+
+-export([open/0, close/0, write_page/1, delete_page/1, publish_page/1, unpublish_page/1])
+
+open() -> application:start(book).
+
+close() -> application:stop(book).
+
+write_page(Draft) ->
+
+delete_page(Draft) ->
+
+publish_page(Draft) ->
+
+unpublish_page(Draft) ->
