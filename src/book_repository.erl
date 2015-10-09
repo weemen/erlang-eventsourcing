7d08ec6e97c47eb10e106d8b45d6502fabe3ab2b
Leon Weemen <l.weemen@simgroep.nl>
2015-10-09 08:58:03 +0200

    Added skeleton for book repository

---
+++ src/book_repository.erl
@@ -0,0 +1,9 @@
+-module(book_repository).
+
+-export([get_by_id/1, save/1, add_to_cache/1, remove_from_cache]).
+
+%%get_by_id(Id) ->
+%%save(Pid) ->
+%%add_to_cache(Id) ->
+%%remove_from_cache(Id) ->
+%%load_from_event_story(Id) ->
