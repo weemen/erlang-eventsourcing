7ae71954ce491dd53f1abb6884bf6cbf2fbaae18
Leon Weemen <l.weemen@simgroep.nl>
2015-10-09 08:48:33 +0200

    Added base methods for command handler, no implementation

--- src/book_command_handler.erl
+++ src/book_command_handler.erl
@@ -2,7 +2,9 @@
 -author("weemen").
 
 %% API
--export([add_handler/0, delete_handler/0, init/1]).
+-export([add_handler/0, delete_handler/0, init/1, handle_event/2, handle_call/2, handle_info/2]).
+
+-include("commands.hrl").
 
 add_handler() ->
   book_command_bus:add_handler(?MODULE, []).
@@ -12,3 +14,18 @@ delete_handler() ->
 
 init([]) ->
   {ok, []}.
+
+%% handle_event(#write_new_draft{id=Id}, State) ->
+%% handle_event(#delete_draft{id=Id}, State) ->
+%% handle_event(#refine_draft{id=Id}, State) ->
+%% handle_event(#publish_draft{id=Id}, State) ->
+%% handle_event(#unpublish_draft{id=Id}, State) ->
+
+handle_event(_, State) ->
+    {ok, State}.
+
+handle_call(_, State) ->
+    {ok, State}.
+
+handle_info(_, State) ->
+    {ok, State}
