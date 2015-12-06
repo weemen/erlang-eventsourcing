7fc2ac4e81526d9e078ecb902fb633783d57925b
Leon Weemen <weemen@leonweemen.nl>
2015-12-06 10:10:06 +0100

    Added new command and new event

--- include/blog_data_structures.hrl
+++ include/blog_data_structures.hrl
@@ -3,8 +3,18 @@
   id
 }).
 
+-record(refine_title_of_draft,{
+  id,
+  title
+}).
+
 %% Events
 -record(new_draft_made, {
   id,
   date_created
+}).
+
+-record(title_of_draft_refined,{
+  id,
+  title
 }).
