974f422c199fe5e14d5710ac2f5da722b7b3835e
Leon Weemen <weemen@leonweemen.nl>
2015-12-06 12:16:07 +0100

    Added writelines for tracking

--- src/keypid.erl
+++ src/keypid.erl
@@ -4,6 +4,7 @@
 -define(TABLE_ID, ?MODULE).
 
 init() ->
+  io:fwrite("caching feature keypid initialized!~n"),
   ets:new(?TABLE_ID, [public, named_table]),
   ok.
 
@@ -11,6 +12,7 @@ delete(Key) ->
   ets:delete(?TABLE_ID, Key).
 
 save(Key, Pid) ->
+  io:fwrite("Saving Key: ~p to PID: ~p~n~n", [Key, Pid]),
   save_helper(Key, Pid, is_pid(Pid)).
 
 save_helper(Key, Pid, true) ->
@@ -19,13 +21,18 @@ save_helper(_, _, _) ->
   false.
 
 get(Key) ->
+  io:fwrite("Looking up Key: ~p~n", [Key]),
   case ets:lookup(?TABLE_ID, Key) of
     [{Key, Pid}] ->
       case is_pid(Pid) andalso is_process_alive(Pid) of
         true ->
+          io:fwrite("Pid found: ~p~n", [Pid]),
           Pid;
         false ->
+          io:fwrite("Pid is DEAD??!~n"),
           not_found
       end;
-    [] -> not_found
+    [] ->
+      io:fwrite("Pid NOT found~n"),
+      not_found
   end.
